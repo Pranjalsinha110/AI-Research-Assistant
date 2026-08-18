ee62ba9c75654b4246ed70c474f10b22eec3f6c7
pranjal <sinhapranjal2005@gmail.com>
2026-08-18 14:59:26 +0530

    changed model to tool calling supported model

--- Backend/model/model.py
+++ Backend/model/model.py
@@ -3,6 +3,6 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-llm = ChatGroq(model="groq/compound-mini",
+llm = ChatGroq(model="openai/gpt-oss-120b",
                temperature=0.7
                )
